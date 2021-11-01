4d25f0f63418de1c6876350bde25df0563bd93b2
jamsh1dbe <jamshidbeksharipov2@gmail.com>
2021-11-01 15:09:54 +0500

    test

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+// 1-masala
+
+// var a = +'230.000'
+// function number(){
+//         b = a.toString();
+//         console.log(b);
+//     }
+// number()
+    
+    // var a =+'00402';
+    // function number(){
+        //     b = a.toString()
+        //     console.log(b);
+        // }
+        // number()
+        
+        // var a =+'03.1400'
+        // function number(){
+            //     b = a.toString()
+            //     console.log(b);
+            // }
+            // number()
+    // var a= +'30';
+    // function number(){
+    //     b =a.toString()
+    //     console.log(b);
+    // }
+    // number()
+
+    // 2-masala
+    var a = ["@","@","@","@"]
+    const text=()=> {
+        for (let i = 0;i < a.length; i++) {
+            var count = (i === i);
+            if(count=true){
+                console.log(a,'true');
+            }
+            else {
+                console.log(a,'false');
+            }
+            
+        }
+
+    }
+    text(a)
+
+
+    // var a = []
+
+    // const text=()=> {
+    //         for (let i = 0;i < a.length; i++) {
+    //             // var count = (i === i);
+    //             if(i===i){
+    //                 console.log(a,'true');
+    //             }
+    //             else {
+    //                 console.log(a,'false');
+    //             }
+                
+    //         }
+    
+    //     }
+    //     text(a)
+
+    let i =0;
+    while(i<=5){
+        i++
+        console.log(i);
+    }
